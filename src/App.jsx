ac3bc883ec508d1c995d43ef9066f2f2d66ba656
Oleksandr Konovalenko <00mp5@bbzolten.ch>
2026-02-03 08:42:38 +0100

    homework 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import imgUrl from './porsche.jpg';
 
-const favouriteBooks = [
- { id: "id-1", name: "JS for beginners" },
- { id: "id-2", name: "React basics" },
- { id: "id-3", name: "React Router overview" },
- { id: "id-4", name: "Redux in depth" },
-];
+const myName = 'Oleksandr';
+const favWebsite = {
+  name: 'ETH Zurich',
+  url: 'https://ethz.ch/en.html'
+};
+const a = 154625;
+const b = 35422552;
 
-function renderBooks(books) {
-    return books.map((book) => (<li key={book.id}>{book.name}</li>));
+const colors = ['red', 'green', 'blue', 'yellow', 'black', 'white'];
+
+const sumFunction = (x, y) => {
+  return x + y;
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (<>
-    <h1>Test</h1>
-    <ul className='test'>
-      {renderBooks(favouriteBooks)}
+    <h1>{myName}</h1>
+    <p>Ласкаво просимо до нашого сайту!</p>
+    <img src={imgUrl} alt="Porsche 911" />
+    <a href={favWebsite.url} className='myFavWebsiteLink'>My favorite website: {favWebsite.name}</a>
+    <p className='resultOfSum'>Sum of {a} and {b} is {sumFunction(a, b)}</p>
+    <ul>
+      {colors.map((color, index) => (
+        <li key={index} style={{ color: color }}>{color}</li>
+      ))}
     </ul>
     </>
   )
